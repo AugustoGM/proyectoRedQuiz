2cec7f899c615a0f334453e2f01f547a422ac462
JacquiVA02 <a01736339@tec.mx>
2023-10-31 19:44:43 -0600

    MiPuntaje_Porcentajes

---
+++ app/src/main/java/com/example/proyectoredquiz/miavatar.java
@@ -0,0 +1,14 @@
+package com.example.proyectoredquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class miavatar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_miavatar);
+    }
+}
