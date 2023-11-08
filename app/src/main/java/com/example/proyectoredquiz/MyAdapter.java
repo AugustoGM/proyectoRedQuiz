ce10a1077efcd4c6025b1b2d4a5236923d29f1f4
PamelaHernandezMontero <A01736368@tec.mx>
2023-11-08 12:48:11 -0600

    delete_incompleto

--- app/src/main/java/com/example/proyectoredquiz/MyAdapter.java
+++ app/src/main/java/com/example/proyectoredquiz/MyAdapter.java
@@ -1,19 +1,27 @@
 package com.example.proyectoredquiz;
 
+import static com.google.firebase.database.DatabaseKt.getSnapshots;
+
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.auth.User;
 
 import java.util.ArrayList;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    private FirebaseFirestore mFirestore = FirebaseFirestore.getInstance();
     Context context;
     ArrayList<Question> list;
 
@@ -31,10 +39,33 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        DocumentSnapshot documentSnapshot = getSnapshots().getSnapshot(holder.getAdapterPosition());
+        final String id = documentSnapshot.getId();
+
         Question question = list.get(position);
         holder.pregunta.setText(question.getPregunta());
         holder.categoria.setText(question.getCategoria());
 
+        holder.btn_eliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deletePregunta(id);
+            }
+        });
+    }
+
+    private void deletePregunta(String id) {
+        mFirestore.collection("preguntas").document(id).delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
     }
 
     @Override
@@ -44,10 +75,14 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
         TextView pregunta, categoria;
+        ImageView btn_eliminar;
+
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             pregunta = itemView.findViewById(R.id.textPregunta);
             categoria = itemView.findViewById(R.id.textcategoria);
+
+            btn_eliminar = itemView.findViewById(R.id.btn_delete);
         }
     }
 }
