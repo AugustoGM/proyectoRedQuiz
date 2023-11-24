86f6eb2d0c768733945cf9f32ff6e02e43eaac8e
JacquiVA02 <a01736339@tec.mx>
2023-11-24 14:07:44 -0600

    fotoPerfil_Administrador

--- app/src/main/java/com/example/proyectoredquiz/MenuAdministrador.java
+++ app/src/main/java/com/example/proyectoredquiz/MenuAdministrador.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -15,6 +16,7 @@ public class MenuAdministrador extends AppCompatActivity {
     Button btn_ver;
     Button btn_add, btn_cerrar;
     FirebaseAuth mAuth;
+    ImageView foto, editar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +24,14 @@ public class MenuAdministrador extends AppCompatActivity {
         setContentView(R.layout.activity_menu_administrador);
         mAuth = FirebaseAuth.getInstance();
 
+        foto = findViewById(R.id.fotoAdmin);
+        editar = findViewById(R.id.editarFA);
         btn_add = findViewById(R.id.btn_addPregunta);
         btn_ver = findViewById(R.id.btn_ver);
         btn_cerrar = findViewById(R.id.btn_cerrar2);
+
+        foto.setImageResource(R.drawable.profilemen);
+        editar.setVisibility(View.GONE);
 
         btn_ver.setOnClickListener(new View.OnClickListener() {
             @Override
